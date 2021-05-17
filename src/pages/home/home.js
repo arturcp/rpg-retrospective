b0a629f7e8015469e7ea76baa39e2dcc97ccea47
Artur Caliendo Prado <artur.prado@gmail.com>
2021-05-17 08:48:48 -0300

    Create character name step

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,9 +3,10 @@ import React, { useState } from 'react';
 import Header from '../../components/Header/Header';
 import Login from '../../components/Login/Login';
 import CharacterSelection from '../../components/CharacterSelection/CharacterSelection';
+import CharacterName from '../../components/CharacterName/CharacterName';
 
 const Home = () => {
-  const [stage, setStage] = useState('loggedIn')
+  const [stage, setStage] = useState('initial')
 
   const changeStage = (newStage) => {
     setStage(newStage);
@@ -15,11 +16,15 @@ const Home = () => {
     <div>
       <Header />
       {stage === 'initial' && (
-        <Login onClick={() => { changeStage('loggedIn') }}/>
+        <Login onClick={() => { changeStage('loggedIn') }} />
       )}
 
       {stage === 'loggedIn' && (
-        <CharacterSelection />
+        <CharacterSelection onClick={() => { changeStage('characterSelected') }} />
+      )}
+
+      {stage === 'characterSelected' && (
+        <CharacterName onClick={() => { changeStage('characterNameSaved') }} />
       )}
     </div>
   );
