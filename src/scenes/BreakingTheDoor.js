35f3ca7f1614e68da46042fcad5b286c588e680e
Artur Caliendo Prado <artur.prado@gmail.com>
2021-05-21 17:37:33 -0300

    Add effect to show that the door was broken (#27)

---
+++ src/scenes/BreakingTheDoor.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import Scenario from '../components/Scenario/Scenario';
+import Player from '../components/Sprites/Player';
+import CONSTANTS from '../domain/constants';
+import GlitchClip from 'react-glitch-effect/core/GlitchClip';
+
+const BreakingTheDoor = (props) => {
+  useEffect(() => {
+    setTimeout(() => props.onSceneEnd(), 4000);
+  }, []);
+
+  const { character } = props;
+
+  return (
+    <GlitchClip>
+      <Scenario scene="gateway">
+        <Player
+          image={character.avatar}
+          data={CONSTANTS.SPRITE_DIMENSIONS}
+          allowInteraction={false}
+          initialData={{
+            position: { x: 215, y: 225 },
+            direction: CONSTANTS.DIRECTIONS.UP,
+            step: CONSTANTS.MOVEMENT.STOPPED,
+          }}
+        />
+      </Scenario>
+    </GlitchClip>
+  )
+};
+
+export default BreakingTheDoor;
