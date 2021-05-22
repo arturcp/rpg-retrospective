fbb89a0c2cb2b64f1c898b0d5d114389434c5a3e
Artur Caliendo Prado <artur.prado@gmail.com>
2021-05-22 09:51:26 -0300

    Create modal, backdrop and loading

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Backdrop from '../Backdrop/Backdrop';
+import { Button } from 'antd';
+import './styles.scss';
+
+// Usage example:
+//
+// <Modal
+//   buttonText="Close"
+//   title="My first modal"
+//   onButtonClick={() => { console.log('clicked'); }}
+//   >
+//   Here comes a text
+// </Modal>
+const modal = (props) => {
+  const { onButtonClick, title, buttonText } = props;
+
+  return (
+    <>
+      <Backdrop />
+      <div className="modal-container">
+        <h3 className="modal-header">
+          {title}
+        </h3>
+        <div className="modal-body">
+          {props.children}
+        </div>
+        <div className="modal-actions">
+          <Button onClick={onButtonClick}>
+            {buttonText}
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+modal.propTypes = {
+  // The title that will appear at the top of the
+  // modal. It will be inserted into a h3 tag.
+  title: PropTypes.string,
+
+  // Text of the button that is positioned at the
+  // bottom right of the modal.
+  buttonText: PropTypes.string,
+
+  // Function that will be triggered when the
+  // action button is clicked.
+  onButtonClick: PropTypes.func,
+};
+
+export default modal;
--- src/pages/common-room/common-room.js
+++ src/pages/common-room/common-room.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
+
+import Modal from '../../components/UI/Modal/Modal';
+import loading from '../../images/loading.gif';
 
 import './styles.scss';
 
-const commonRoom = () => {
-  return (
-    <div className="container">
-    </div>
-  )
-};
+const client = new W3CWebSocket('ws://127.0.0.1:8000');
 
-export default commonRoom;
+export default class CommonRoom extends Component {
+  state = {
+    connected: false,
+    showModal: true,
+  }
+
+  componentDidMount() {
+    client.onopen = () => {
+      this.setState({ connected: true })
+      console.log('Websocket client connected');
+    };
+
+    client.onmessage = (message) => {
+      const dataFromServer = JSON.parse(message.data);
+      // console.log('Get a reply: ', dataFromServer);
+
+      // if (dataFromServer.type === 'message') {
+      //   this.saveReceivedMessage(dataFromServer);
+      // }
+    };
+  }
+
+  render() {
+    const { showModal } = this.state;
+
+    return (
+      <div className="container">
+        {showModal && (
+          <Modal
+            buttonText="Next"
+            title=""
+            onButtonClick={() => { console.log('clicked'); }}
+          >
+            <img src={loading} alt="loading"/>
+          </Modal>
+        )}
+      </div>
+    )
+  }
+};
