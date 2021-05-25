199a572301d3254303aa0c2301897d85eecc1287
Artur Caliendo Prado <artur.prado@gmail.com>
2021-05-25 09:06:28 -0300

    Make websocket work with players

--- src/pages/common-room/common-room.js
+++ src/pages/common-room/common-room.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import Modal from '../../components/UI/Modal/Modal';
 import loadingImage from '../../images/loading.gif';
 import Player from '../../components/Sprites/Player';
+import Actor from '../../components/Sprites/Actor';
 import CONSTANTS from '../../domain/constants';
 import { characters } from '../../domain/characters';
 import { Input } from 'antd';
@@ -176,33 +177,48 @@ class CommonRoom extends Component {
 
   buildPlayer = (player) => {
     const character = characters[player.character.type];
+    const currentPlayer = player.userID === this.state.userID;
 
     console.log(`Drawing ${player.character.name} at ${player.position.x}, ${player.position.y} (${player.direction})`)
 
-    return <Player
-      key={player.character.name}
-      // key={player.userID}
-      image={character.avatar}
-      data={CONSTANTS.SPRITE_DIMENSIONS}
-      allowInteraction={player.userID === this.state.userID}
-      initialData={{
-        position: {
-          x: player.position.x,
-          y: player.position.y,
-        },
-        direction: player.direction,
-        step: player.step,
-      }}
-      onMove={(position, direction, step) => {
-        this.sendMessage('player-moved', {
-          userID: player.userID,
-          position,
-          direction,
-          step,
-          character: player.character
-        })
-      }}
-    />;
+    return (
+      <div key={player.character.name}>
+        {currentPlayer && (
+          <Player
+            image={character.avatar}
+            data={CONSTANTS.SPRITE_DIMENSIONS}
+            allowInteraction={true}
+            initialData={{
+              position: {
+                x: player.position.x,
+                y: player.position.y,
+              },
+              direction: player.direction,
+              step: player.step,
+            }}
+            onMove={(position, direction, step) => {
+              this.sendMessage('player-moved', {
+                userID: player.userID,
+                position,
+                direction,
+                step,
+                character: player.character
+              })
+            }}
+          />
+        )}
+
+        {!currentPlayer && (
+          <Actor
+            image={character.avatar}
+            data={CONSTANTS.SPRITE_DIMENSIONS}
+            step={player.step}
+            direction={player.direction}
+            position={player.position}
+          />
+        )}
+      </div>
+    )
   }
 
   render() {
