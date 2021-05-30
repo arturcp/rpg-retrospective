503c5be5dcd90aca65779fb5248c67fb631aab6b
Artur Caliendo Prado <artur.prado@gmail.com>
2021-05-30 09:34:18 -0300

    Create admin page

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // import Home from './pages/home/home';
 import SpeedTest from './pages/speed-test/speed-test';
 import CommonRoom from './pages/common-room/common-room';
+import Admin from './pages/admin/admin';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       {/* <Route exact path="/" component={Home} /> */}
       <Route exact path="/" component={SpeedTest} />
+      <Route exact path="/admin" component={Admin} />
       <Route exact path="/common-room" component={CommonRoom} />
     </Switch>
   </BrowserRouter>
