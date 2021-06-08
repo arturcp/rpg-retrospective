58c6c576f6327be893fb84e40ba1843c55c42262
Artur Caliendo Prado <artur.prado@gmail.com>
2021-06-08 10:46:19 -0300

    Use hash router

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,30 @@
 import React from 'react';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {
+  // BrowserRouter,
+  Switch,
+  Route,
+  HashRouter
+} from 'react-router-dom';
 
 import Home from './pages/home/home';
 // import SpeedTest from './pages/speed-test/speed-test';
 import CommonRoom from './pages/common-room/common-room';
-// import Admin from './pages/admin/admin';
+import Admin from './pages/admin/admin';
 import QuizResults from './pages/quiz-results/quiz-results';
 
 const Routes = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route exact path="/" component={Home} />
       {/* Use this route to test the common-room without having to navigate all the way
       through the game */}
       {/* <Route exact path="/" component={SpeedTest} /> */}
-      <Route exact path="/control" component={Home} />
+      <Route exact path="/admin" component={Admin} />
       <Route exact path="/common-room" component={CommonRoom} />
       <Route exact path="/quiz-results" render={(props) => <QuizResults {...props} /> } />
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default Routes;
